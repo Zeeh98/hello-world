bb476811eae3c469ea38fd36228ab275e37cdf54
Zeeh98 <54686569+Zeeh98@users.noreply.github.com>
2020-05-09 02:53:43 -0400

    Add files via upload

---
+++ Scatter Plot.R
@@ -0,0 +1,2 @@
+plot(food$healthy_feeling, food$eating_out, main = "The Health of Students That Eat Outside", xlab = "Rating (From 1-10)", ylab = "Eating Outside Food" )
+cor(food$eating_out, food$healthy_feeling)
